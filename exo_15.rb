a18cfec9f034114a9baf046139b61c7cb622da8b
0xMickq <mickqdev@gmail.com>
2025-03-11 15:21:00 +0100

    adding the 15th exercice

---
+++ exo_15.rb
@@ -0,0 +1,11 @@
+puts "Choisissez un nombre entre 1 et 25 :"
+print ">"
+user_number_choice = gets.chomp.to_i 
+if user_number_choice < 1 || user_number_choice > 25
+    puts "Tant pis t'aura pas ta pyramide t'avais qu'a lire la consigne ^^"
+else 
+    puts "Tadaaaa :"
+    (1..user_number_choice).each do |i|
+        puts '#' * i
+    end
+end
